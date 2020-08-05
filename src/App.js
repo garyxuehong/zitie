18a7c0ec310f400041584d496dd02b2761676c06
Gary Xue <Gary.Xue@reece.com.au>
2020-08-05 21:09:44 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const sample = `《咏鹅》
+唐·骆宾王
+鹅，鹅，鹅，
+曲项向天歌。
+白毛浮绿水，
+红掌拨清波。 
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="source">
+        <textarea readOnly={true} value={sample} />
+      </div>
+      <div className="preview">
+        {sample.split("\n").map((l, i) => (
+          <Line key={i} line={l} />
+        ))}
+      </div>
     </div>
   );
+}
+
+function Line({ line }) {
+  return (
+    <div className="line">
+      {line.split("").map((c, i) => (
+        <Cell key={i} ch={c} />
+      ))}
+    </div>
+  );
+}
+
+function Cell({ ch }) {
+  return <div className="cell">{ch}</div>;
 }
 
 export default App;
